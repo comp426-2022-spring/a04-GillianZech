a7ff476e1a78f164180322328fe839f8c6d48331
Gillian Zech <ginger01@email.unc.edu>
2022-04-27 18:56:56 -0400

    deleting

--- server.js
+++ server.js
@@ -66,87 +66,41 @@ app.get('/app', (req, res) => {
 
 // app.post("/app/new/user", (req, res, next) => {
 
-// app.use( (req, res, next) => {
-//     let logdata = {
-//         remoteaddr: req.ip,
-//         remoteuser: req.user,
-//         time: Date.now(),
-//         method: req.method,
-//         url: req.url,
-//         protocol: req.protocol,
-//         httpversion: req.httpVersion,
-//         status: req.statusCode,
-//         referer: req.headers['referer'],
-//         useragent: req.headers['user-agent']
-//     }
-//     const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
-//     const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
-//     res.status(200).json(info)
-//     next()
-// });
-
-// app.get("/app/log/access", (req, res, next) => {
-//     if (debug) {
-//         try {
-//             console.log("1")
-//             const stmt = db.prepare('SELECT * FROM accesslog').all()
-//             console.log("2")
-//             res.status(200).json(stmt) // error on this line
-//             console.log("3")
-//             console.log(stmt)
-//             console.log("4")
-        
-//         } catch {
-//             console.error("")
-//         }
-//     }
-// })
-
-
-
-
-
-
-
 app.use( (req, res, next) => {
-    if (debug) {
-        let logdata = {
-            remoteaddr: req.ip,
-            remoteuser: req.user,
-            time: Date.now(),
-            method: req.method,
-            url: req.url,
-            protocol: req.protocol,
-            httpversion: req.httpVersion,
-            status: req.statusCode,
-            referer: req.headers['referer'],
-            useragent: req.headers['user-agent']
-        }
-        const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
-        const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
-        res.status(200).json(info)
-        next()
-        app.get("/app/log/access", (req, res, next) => {
-            try {
-                console.log("1")
-                const stmt = db.prepare('SELECT * FROM accesslog').all()
-                console.log("2")
-                res.status(200).json(stmt) // error on this line
-                console.log("3")
-                console.log(stmt)
-                console.log("4")
-            
-            } catch {
-                console.error("")
-            }
-        })
+    let logdata = {
+        remoteaddr: req.ip,
+        remoteuser: req.user,
+        time: Date.now(),
+        method: req.method,
+        url: req.url,
+        protocol: req.protocol,
+        httpversion: req.httpVersion,
+        status: req.statusCode,
+        referer: req.headers['referer'],
+        useragent: req.headers['user-agent']
     }
+    const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
+    const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
+    res.status(200).json(info)
+    next()
 });
 
-
-
-
-
+app.get("/app/log/access", (req, res, next) => {
+    if (debug) {
+        try {
+            console.log("1")
+            const stmt = db.prepare('SELECT * FROM accesslog').all()
+            console.log("2")
+            res.status(200).json(stmt) // error on this line
+            console.log("3")
+            console.log(stmt)
+            console.log("4")
+        
+        } catch {
+            console.error("")
+        }
+    }
+})
 
 
 
